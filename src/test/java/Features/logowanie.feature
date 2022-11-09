b718a9d6cd12a69983e6e60153c1cb3832f45e94
ahrian87 <maciej.zbydniewski@gmail.com>
2022-11-09 20:09:24 +0100

    bdd1 - dodane wysokie poziomy

--- src/test/java/Features/logowanie.feature
+++ src/test/java/Features/logowanie.feature
@@ -2,10 +2,26 @@ Feature: Logowanie do aplikacji
 
   Jako użytkownik chcę mieć możliwość zalogowania się do aplikacji, aby móc skorzystać z zaawansowanych funckji systemu
 
+
   Scenario: Poprawne logowanie do aplikacji
-    Given Uzytkownik otwiera przegladarke
-    And Uzytkownik wpisuje adres https://the-internet.herokuapp.com/login
+    Given Uzytkownik wpisuje adres https://the-internet.herokuapp.com/login
     When Uzytkownik wpisuje poprawny login
     And Uzytkownik wpisuje poprawne haslo
+    And Uzytkownik klika przycisk Login
+    Then Uzytkownik zostal poprawnie zalogowany
+
+
+  Scenario: Niepoprawne logowanie do aplikacji
+    Given Uzytkownik wpisuje adres https://the-internet.herokuapp.com/login
+    When Uzytkownik wpisuje poprawny login
+    And Uzytkownik wpisuje niepoprawne haslo
+    And Uzytkownik klika przycisk Login
+    Then Uzytkownik nie zostal poprawnie zalogowany
+
+    @now
+  Scenario: Poprawne logowanie do aplikacji (druga wersja)
+    Given Uzytkownik wpisuje adres "https://the-internet.herokuapp.com/login"
+    When Uzytkownik wpisuje "tomsmith" w pole username
+    And Uzytkownik wpisuje "SuperSecretPassword!" w pole password
     And Uzytkownik klika przycisk Login
     Then Uzytkownik zostal poprawnie zalogowany
--- src/test/java/Steps/Logowanie.java
+++ src/test/java/Steps/Logowanie.java
@@ -1,5 +1,8 @@
 package Steps;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,20 +15,48 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Logowanie {
 
-    WebDriver driver;
+    static WebDriver driver;
+
+    @BeforeAll
+    public static void setDriver() {
+        System.out.println("Uruchomienie przed wszystkimi scenariuszami testowymi");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Before
+    public void before() {
+        System.out.println("Uruchomienie przed każdym scenariuszem testowym");
+    }
 
     @Given("Uzytkownik otwiera przegladarke")
     public void testUzytkownikOtwieraPrzegladarke() {
         System.out.println("Uzytkownik otworzyl przegladarke");
         // System.setProperty("webdriver.chrome.driver", "c:/chromedriver/chromedriver107.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
     }
 
     @Given("Uzytkownik wpisuje adres https:\\/\\/the-internet.herokuapp.com\\/login")
     public void uzytkownik_wpisuje_adres_https_the_internet_herokuapp_com_login() {
         System.out.println("Uzytkownik wpisuje adres strony");
         driver.navigate().to("https://the-internet.herokuapp.com/login");
+    }
+
+    // Given Uzytkownik wpisuje adres "https://the-internet.herokuapp.com/login"
+    @Given("Uzytkownik wpisuje adres {string}")
+    public void uzytkownik_wpisuje_adres(String url) {
+        driver.navigate().to(url);
+    }
+
+    // When Uzytkownik wpisuje "tomsmith" w pole username
+    @When("Uzytkownik wpisuje {string} w pole username")
+    public void uzytkownik_wpisuje_w_pole_username(String username) {
+        driver.findElement(By.id("username")).sendKeys(username);
+    }
+
+    // When Uzytkownik wpisuje "SuperSecretPassword!" w pole password
+    @When("Uzytkownik wpisuje {string} w pole password")
+    public void uzytkownik_wpisuje_w_pole_password(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
     }
 
     @When("Uzytkownik wpisuje poprawny login")
@@ -51,6 +82,22 @@ public class Logowanie {
     public void uzytkownik_zostal_poprawnie_zalogowany() {
         System.out.println("Uzytkownik zostaje poprawnie zalogowany");
         Assert.assertEquals("https://the-internet.herokuapp.com/secure", driver.getCurrentUrl());
+    }
+
+    @When("Uzytkownik wpisuje niepoprawne haslo")
+    public void uzytkownik_wpisuje_niepoprawne_haslo() {
+        System.out.println("Uzytkownik wpisuje niepoprawne haslo");
+        driver.findElement(By.id("password")).sendKeys("aaa");
+    }
+
+    @Then("Uzytkownik nie zostal poprawnie zalogowany")
+    public void uzytkownik_nie_zostal_poprawnie_zalogowany() {
+        System.out.println("Uzytkownik nie zostal zalogowany");
+        Assert.assertEquals("https://the-internet.herokuapp.com/login",driver.getCurrentUrl());
+    }
+
+    @AfterAll
+    public static void tearDown() {
         driver.close();
     }
 
